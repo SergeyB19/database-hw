892dd9c072fd33191b5b1e058bb4e82ee1ee3156
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-10-03 13:50:43 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course3/databasehw/DatabaseHwApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.databasehw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
