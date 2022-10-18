b5fea2192db514db3db2074f099f84d9d0030449
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-10-18 18:03:54 +0300

    hw

--- src/main/java/pro/sky/java/course3/databasehw/repository/StudentRepository.java
+++ src/main/java/pro/sky/java/course3/databasehw/repository/StudentRepository.java
@@ -1,9 +1,21 @@
 package pro.sky.java.course3.databasehw.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pro.sky.java.course3.databasehw.model.Student;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+import java.util.List;
 
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query(value = "SELECT *, FROM student", nativeQuery = true)
+    Student getAllByStudents(int age);
+
+    @Query(value = "SELECT * FROM student GROUP BY age, nativeQuery = true)
     Student findByAgeBetween(int age);
+
+    @Query(value = "SELECT * FROM student OFFSET 5 , nativeQuery = true)
+            Student findByAgeBetween(int age);
+
 }
+
+
